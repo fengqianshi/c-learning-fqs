89710063d221c6480fcd1f9c2c6700e30876272c
Feng Qianshi <6684774@qq.com>
2017-03-06 20:32:39 +0800

    upload

---
+++ week1/first - 副本.c
@@ -0,0 +1,37 @@
+void main() {
+
+
+    printf("                   …œ\n" );
+    printf("                   …œ\n" );
+    printf("                   …œ\n" );
+    printf("                   …œ\n" );
+    printf("                   …œ\n" );
+    printf("                   …œ…œ…œ…œ…œ\n" );
+    printf("                   …œ\n" );
+    printf("                   …œ\n" );
+    printf("                   …œ\n" );
+    printf("                   …œ\n" );
+    printf("                   …œ\n" );
+    printf("        …œ…œ…œ…œ…œ…œ…œ…œ…œ…œ…œ…œ\n" );
+
+    printf("        \n\n\n\n\n" );
+
+    printf("                “º\n" );
+    printf("                “º\n" );
+    printf("        “º“º“º“º“º“º“º“º“º\n" );
+    printf("                “º\n" );
+    printf("            “º“º“º“º“º\n\n" );
+    printf("  “º“º“º“º“º“º“º“º“º“º“º“º“º“º“º\n" );
+    printf("  “º                          “º\n" );
+    printf("  “º      “º“º“º“º“º“º“º     “º \n\n" );
+
+
+
+
+
+
+
+
+
+    return;
+}
---
+++ week1/first.c
@@ -0,0 +1,38 @@
+void main() {
+
+
+    printf("                   上\n" );
+    printf("                   上\n" );
+    printf("                   上\n" );
+    printf("                   上\n" );
+    printf("                   上\n" );
+    printf("                   上上上上上\n" );
+    printf("                   上\n" );
+    printf("                   上\n" );
+    printf("                   上\n" );
+    printf("                   上\n" );
+    printf("                   上\n" );
+    printf("        上上上上上上上上上上上上\n" );
+
+    printf("        \n\n\n\n\n" );
+
+    printf("                F                \n" );
+    printf("                F                \n" );
+    printf("        FFFFFFFFFFFFFFFFFF\n"        );
+    printf("                F                 \n");
+    printf("            FFFFFFFFFF          \n\n");
+    printf("  FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF\n" );
+    printf("  ff                           ff\n" );
+    printf("  ff    ffffffffffffffffff   ff\n\n" );
+    printf("        ffffffffffffffffff       \n" );
+    printf("        ff              fF       \n" );
+    printf("        ff              fF       \n" );
+    printf("        ffffffffffffffffff     \n\n" );
+    printf("          ff          ff         \n" );
+    printf("             ff    ff            \n" );
+    printf("  FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF\n" );
+
+
+
+    return;
+}
